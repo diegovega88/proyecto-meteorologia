732f5190b6f67b7e7eccc1bab13fb43598ff2b49
Diego Vega <diego.sv.0225@gmail.com>
2026-04-26 20:55:58 +0200

    Add utility function for temperature conversion

---
+++ src/weather/utils.py
@@ -0,0 +1,10 @@
+def celsius_to_fahrenheit(temp_c: float) -> float:
+    """Convert a temperature from Celsius to Fahrenheit.
+
+    Args:
+        temp_c: The temperature in degrees Celsius.
+
+    Returns:
+        The temperature in degrees Fahrenheit.
+    """
+    return (temp_c * 9 / 5) + 32
